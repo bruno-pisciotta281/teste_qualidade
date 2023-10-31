1b456dade54dfd05a5a81d760e4738d96f4e4b0d
Bruno Pisciotta <52466841+bruno-pisciotta281@users.noreply.github.com>
2023-10-31 11:28:21 -0300

    Delete jokenpo.py

--- jokenpo.py
+++
@@ -1,7 +0,0 @@
-def jokenpo(jogada1, jogada2):
-    if jogada1 == jogada2:
-        return "Empate"
-    elif (jogada1 == "Pedra" and jogada2 == "Tesoura") or (jogada1 == "Tesoura" and jogada2 == "Papel") or (jogada1 == "Papel" and jogada2 == "Pedra"):
-        return "Jogador 1 vence"
-    else:
-        return "Jogador 2 vence"
